76d053cd062fff59aae95d69aaacfbfd47366431
David Meyer <dihm@users.noreply.github.com>
2022-12-01 17:00:23 -0500

    Add some utility functions

--- src/leveldiagram/__init__.py
+++ src/leveldiagram/__init__.py
@@ -1,3 +1,5 @@
 # import API elements here
 
+from .utils import ket_str, bra_str
+
 __version__ = '0.0.1'
---
+++ src/leveldiagram/utils.py
@@ -0,0 +1,44 @@
+"""
+Miscellaneous utility functions
+"""
+
+
+def ket_str(s):
+    """
+    Put a ket around the string in matplotlib.
+    """
+
+    in_s = str(s)
+
+    out_s = '$\\left|' + in_s + '\\right\\rangle$'
+
+    return out_s
+
+
+def bra_str(s):
+    """
+    Put a bra around the string in matplotlib.
+    """
+
+    in_s = str(s)
+
+    out_s = '$\\left\\langle' + in_s + '\\right|$'
+
+    return out_s
+
+
+def deep_update(mapping, *updating_mappings):
+    """
+    Helper function to update nested dictionaries.
+
+
+    Lifted from pydantic
+    """
+    updated_mapping = mapping.copy()
+    for updating_mapping in updating_mappings:
+        for k, v in updating_mapping.items():
+            if k in updated_mapping and isinstance(updated_mapping[k], dict) and isinstance(v, dict):
+                updated_mapping[k] = deep_update(updated_mapping[k], v)
+            else:
+                updated_mapping[k] = v
+    return updated_mapping
